5e88a51d3783914b47d9ba03e5f2b2aade6859b5
BadetiSravaniSatya <74294142+BadetiSravaniSatya@users.noreply.github.com>
2020-11-17 09:03:29 +0530

    Add files via upload

---
+++ src/Ascii.java
@@ -0,0 +1,12 @@
+public class Ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ch1 = 'a';  
+		int ch2 = 'b';  
+		System.out.println("The ASCII value of a is: "+ch1);  
+		System.out.println("The ASCII value of b is: "+ch2);  
+	}
+
+	}
+
